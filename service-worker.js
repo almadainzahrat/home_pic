49f7000036d66eb2e668eab1722cf806690d23b3
almadainzahrat <almadainzahrat3@gmail.com>
2025-12-09 15:41:27 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,41 +1,56 @@
-const CACHE_NAME = "madain-cache-v1";
-const BASE = "/home_pic/";
-const FILES_TO_CACHE = [
-  BASE,
-  BASE + "index.html",
-  BASE + "a.mp3",
-  BASE + "maninfest/json"
-
-  // الصور
-  "img/1.png", "img/2.png", "img/3.png", "img/4.png",
-  "img/5.png", "img/6.png", "img/7.png", "img/8.png",
-  "img/9.png", "img/10.png", "img/11.png", "img/12.png",
-  "img/13.png", "img/14.png", "img/15.png", "img/16.png",
-  "img/17.png", "img/18.png", "img/19.png", "img/20.png",
-  "img/21.png", "img/22.png", "img/23.png", "img/24.png",
-  "img/25.png", "img/26.png", "img/27.png", "img/28.png",
-  "img/29.png", "img/30.png", "img/31.png", "img/32.png",
-  "img/33.png", "img/34.png", "img/35.png", "img/36.png",
-  "img/37.png", "img/38.png", "img/39.png", "img/40.png",
-  "img/41.png", "img/42.png", "img/43.png", "img/44.png",
-  "img/45.png", "img/46.png", "img/47.png", "img/48.png",
-  "img/49.png", "img/50.png", "img/51.png", "img/52.png",
-  "img/53.png", "img/54.png", "img/55.png", "img/56.png",
-  "img/57.png", "img/58.png", "img/59.png",
-
-  "manifest.json"
+const CACHE_NAME = 'gallery-pwa-cache-v1';
+const urlsToCache = [
+  '/',
+  '/gallery.html',
+  '/manifest.json',
+  '/a.mp3',
+  '/img/icon-192x192.png',
+  '/img/icon-512x512.png'
 ];
 
-self.addEventListener("install", event => {
+// Add all 59 image paths to the cache list
+for (let i = 1; i <= 59; i++) {
+    urlsToCache.push(`/img/${i}.png`);
+}
+
+// The install handler takes care of precaching the resources we always need.
+self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
   );
 });
 
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(resp => {
-      return resp || fetch(event.request);
+// The activate handler takes care of cleaning up old caches.
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
     })
+  );
+});
+
+// The fetch handler serves up the cached content if available, falling back to the network.
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
   );
 });
